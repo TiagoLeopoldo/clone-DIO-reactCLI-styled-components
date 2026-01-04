6a691c35721c086d28a59aaeae7d5e4c50ea2f1e
TiagoLeopoldo <tnleopoldo.dev@gmail.com>
2026-01-03 21:52:21 -0300

    refactor: migrate project from JavaScript to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.js'
 import { GlobalStyle } from './styles/global.js'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <GlobalStyle />
     <App />
